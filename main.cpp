663be02c15fa67eb9ac80e035bc8689a041f3988
liajaned <117766962+liajaned@users.noreply.github.com>
2024-03-09 14:00:54 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+    // Print standard output
+    // on the screen
+    cout << "DOING PIPELINE PES1UG21CS428";
+ 
+    return 0;
+}
